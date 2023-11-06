6bda09e7ce5bae05ed2bfc0e99724542b7bfebfa
Adame-Qodad <adameqodad341@gmail.com>
2023-11-06 09:46:31 +0100

    -R--_Jeu

--- src/Controller/JeuController.php
+++ src/Controller/JeuController.php
@@ -9,9 +9,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class JeuController extends AbstractController
 {
     #[Route('/jeu', name: 'app_jeu')]
-    public function index(): Response
+    public function listeJeu(): Response
     {
         return $this->render('jeu/listeJeu.html.twig', [
+            'controller_name' => 'JeuController',
+        ]);
+    }
+    #[Route('/ficheJeu', name: 'app_ficheJeu')]
+    public function ficheJeu(): Response
+    {
+        return $this->render('jeu/ficheJeu.html.twig', [
             'controller_name' => 'JeuController',
         ]);
     }
