031139f8bb1b3c1758df2bdbd309d57d1af9114b
Tismaa78 <mathisroussel78@gmail.com>
2023-11-23 14:47:43 +0100

    fiche editeur

--- src/Controller/EditeurController.php
+++ src/Controller/EditeurController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Editeur;
+use App\Repository\EditeurRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EditeurController extends AbstractController
 {
+
     #[Route('/editeur', name: 'app_editeur')]
-    public function listeEditeur(): Response
+    public function listeEditeur(EditeurRepository $repo): Response
     {
-        return $this->render('editeur/listeEditeur.html.twig', [
+        $editeurs=$repo->findAll();
+        $editeurs=array_splice($editeurs, 1);
+        return $this->render('editeur/listeEditeur.html.twig',[
             'controller_name' => 'EditeurController',
+            'lesEditeurs' => $editeurs
+
         ]);
+        
     }
+
+    #[Route('/editeur/{id}', name: 'fiche_editeur')]
+    public function ficheEditeur(Editeur $editeur): Response
+    {
+        return $this->render('editeur/ficheEditeur.html.twig',[
+            'controller_name' => 'EditeurController',
+            'leEditeur' => $editeur
+
+        ]);     
+    }
+
 }
